693bf56adff8f1c9dd338d2b8557ae07e3033f55
Terry Cheng <hello@terrycheng.dev>
2025-10-02 22:45:38 +0800

    chore/plugin: upload plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        // Your provider options here (if any)
+      },
+      settings: {
+        // Change the URL from .media to /uploads
+        url: '/uploads',
+      },
+    },
+  },
+});
