e58c7b85fb0ebc5b483d14038f7369542bf02cd8
Terry Cheng <hello@terrycheng.dev>
2025-10-03 20:26:14 +0800

    chore/config:config update

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,1 @@
-export default ({ env }) => ({
-  upload: {
-    provider: 'local', // or your current provider
-    providerOptions: {
-      // Your provider options here
-    },
-    actionOptions: {
-      upload: {
-        url: (fileName: string) => `${process.env.APP_URL}/uploads/${fileName}`, // Make sure APP_URL is set
-      },
-    },
-  },
-});
+export default () => ({});
